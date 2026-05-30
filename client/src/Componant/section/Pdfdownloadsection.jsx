50ac9f5944f73f244871b4231615c5e7117b59d4
suraj kengar <surajkengar1212@gmail.com>
2026-05-30 16:58:53 +0530

    pdfdownload section added

---
+++ client/src/Componant/section/Pdfdownloadsection.jsx
@@ -0,0 +1,29 @@
+
+
+
+function Pdfdownloadsection(){
+    return (
+        <section id="pdfdownload" className="pdf-section">
+            <div className="pdf-container">
+                <h2>pdf Resource</h2>
+
+                <p className="pdf-description">
+                    Download our complete guide to understand trading basics,risk management , market analysis , and smart strategies in one place
+                </p>
+
+                <div className="pdf-cards">
+                    <div className="pdf-card">
+                        <h3>complete trading guide</h3>
+
+                        <p>one complete pdf covering trading basics , market trends , risk management and smart decision making strategies</p>
+
+                        <button>Download Pdf</button>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Pdfdownloadsection;
