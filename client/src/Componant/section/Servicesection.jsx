61187de19aab376bbdbc58417543e9065c9aa4f2
suraj kengar <surajkengar1212@gmail.com>
2026-05-30 17:00:00 +0530

    servicesection added

---
+++ client/src/Componant/section/Servicesection.jsx
@@ -0,0 +1,49 @@
+
+
+function Servicesection(){
+    return(
+        <section id="services"  className="servicesection">
+            <div className="service-container">
+                <h2>Our Services</h2>
+                <p className="service-description">
+                    We offer practical solutions that help users explore markets, improve
+                    decision-making, and access valuable business resources.
+                </p>
+
+                <div className="service-cards">
+                    <div className="service-card">
+                        <h3>Strategy Consulting</h3>
+                        <p>A social assitant thats flexible can accommodate your schedule and needs , making life easier.</p>
+                    </div>
+
+                    <div className="service-card">
+                        <h3>Financial Advisory</h3>
+                        <p>Modules transform smart trading by automation processes and improving decision-making.</p>
+                    </div>
+
+                    <div className="service-card">
+                        <h3>Management</h3>
+                        <p>There its me , your friendly neighborhood reporters news analyst processes and improving</p>
+                    </div>
+
+                    <div className="service-card">
+                        <h3>Supply optimization</h3>
+                        <p>Hey , have you checket out that new cryptocurrency platform? its pretty cool and easy to use! </p>
+                    </div>
+
+                    <div className="service-card">
+                        <h3>HR consulting</h3>
+                        <p>Hey Guys , just quick update on exchange orders . there have been some changes currency!</p>
+                    </div>
+
+                    <div className="service-card">
+                        <h3>Marketing consulting</h3>
+                        <p>Hey! just wanted to let you know that the price notification module processes is now live....</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Servicesection;
