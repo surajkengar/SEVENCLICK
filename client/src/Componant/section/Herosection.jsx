d6adf1886048a5de89a645f9b1cd7f27c86fcc4f
suraj kengar <surajkengar1212@gmail.com>
2026-05-30 16:57:52 +0530

    hero section added

---
+++ client/src/Componant/section/Herosection.jsx
@@ -0,0 +1,35 @@
+
+
+function Herosection(){
+    return (
+    <section id="home" className="hero-section">
+    <div className="hero-container">
+        <div className="hero-content">
+            <p className="hero-tag">Smart Trading And Business Solution </p>
+
+            <h1>Build a Better Decision With <span>SevenClick</span></h1>
+
+            <p className="hero-description"> 
+                Discover services, articles, downloadable resources, and expert
+                guidance in one modern platform designed for growth.
+            </p>
+
+            <div className="hero-button">
+                <button className="first-btn">Get Started</button>
+                <button className="second-btn">Learn more</button>
+            </div>
+        </div>
+
+        <div className="hero-image">
+            <img 
+                src = "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3"
+                alt = "trading" 
+            />
+        </div>
+    </div>
+    </section>
+
+    )
+}
+
+export default Herosection;
