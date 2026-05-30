22ce1e2fc58961875501c653458087c8ac272615
suraj kengar <surajkengar1212@gmail.com>
2026-05-30 20:11:55 +0530

    app.js update

--- server/src/app.js
+++ server/src/app.js
@@ -15,8 +15,10 @@ app.use(cookieparser());
 
 app.use(
     cors({
-        origin : "http://localhost:5173",
-        credentials : true
+        origin :  process.env.CORS_ORIGIN?.split(",")|| "http://localhost:5173",
+        credentials : true,
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
     })
 )
 
