70651877fc39ef8daf191d167f8cb583c0e08f3a
suraj kengar <surajkengar1212@gmail.com>
2026-05-30 16:02:40 +0530

    management page added

---
+++ client/src/pages/Management.jsx
@@ -0,0 +1,12 @@
+import DashboardLayout from "../Componant/Layout/DashboardLayout";
+
+
+function Management(){
+    return(
+        
+            <h1>Welcome to management page</h1>
+        
+    )
+}
+
+export default Management;
