656f5c7f38faace29ad5c950ecca6ec7a79e554f
suraj kengar <surajkengar1212@gmail.com>
2026-05-30 10:57:30 +0530

    app.js file added

---
+++ server/src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import authroutes from "./routes/authRoutes.js";
+import appointmentrouter from "./routes/Appoitnment.router.js";
+import chatRouter from "./routes/Chat.routes.js";
+import cookieparser from "cookie-parser";
+
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieparser());
+
+app.use(
+    cors({
+        origin : "http://localhost:5173",
+        credentials : true
+    })
+)
+
+app.use("/api/v1/auth" , authroutes);
+app.use("/api/v1/appoitnment" , appointmentrouter);
+app.use("/api/v1/chat" , chatRouter);
+
+app.get("/" ,(req , res) => {
+    res.send("server is running");
+})
+
+
+export default app;
