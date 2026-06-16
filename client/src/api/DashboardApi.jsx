b3cd0e90bb301ca24bd2edd037820d7df85b2adf
suraj kengar <surajkengar1212@gmail.com>
2026-06-16 19:57:36 +0530

    DashboardApi is updated

--- client/src/api/DashboardApi.jsx
+++ client/src/api/DashboardApi.jsx
@@ -9,3 +9,11 @@ export const cancelAppointment = (id)   => API.delete(`/appoitnment/cancleappoit
 export const getChatHistory   = ()     => API.get("/chat/getchathistory");
 export const sendChatMessage  = (data) => API.post("/chat/sendmessage", data);
 export const clearChatHistory = ()     => API.delete("/chat/clearchat");
+
+//--- Adminpanel------------------------
+
+export const getAllUsers = ()=>  API.get("/admin/allusers");
+export const blockUser = (id) => API.patch(`/admin/blockuser/${id}`);
+export const getAllAppointments = ()  => API.get("/admin/allappointments");
+export const updateAppointmentStatus = (id , status) => API.patch(`/admin/appointmentstatus/${id}`,{status});
+export const getAnalytics = () => API.get("/admin/analytics");
