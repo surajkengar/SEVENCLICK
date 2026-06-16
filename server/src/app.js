ecd3233699ca65e04eb0d246fbc2f72626b34b42
suraj kengar <surajkengar1212@gmail.com>
2026-06-16 19:33:58 +0530

    update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -3,15 +3,12 @@ import cors from "cors";
 import authroutes from "./routes/authRoutes.js";
 import appointmentrouter from "./routes/Appoitnment.router.js";
 import chatRouter from "./routes/Chat.routes.js";
+import adminRouter from "./routes/admin.routes.js";
 import cookieparser from "cookie-parser";
 
 
 
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(cookieparser());
 
 app.use(
     cors({
@@ -22,9 +19,15 @@ app.use(
     })
 )
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieparser());
+
+
 app.use("/api/v1/auth" , authroutes);
 app.use("/api/v1/appoitnment" , appointmentrouter);
 app.use("/api/v1/chat" , chatRouter);
+app.use("/api/v1/admin" , adminRouter);
 
 app.get("/" ,(req , res) => {
     res.send("server is running");
